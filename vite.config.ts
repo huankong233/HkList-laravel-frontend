78e88633801e99d5c607e4692a2d650e8183aa8e
huan_kong <2564076459@qq.com>
2023-10-10 15:09:31 +0800

    优化打包结构

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,23 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // minify: 'terser',
+    // terserOptions: {
+    //   compress: {
+    //     // 生产环境时移除console
+    //     drop_console: true,
+    //     drop_debugger: true
+    //   }
+    // }
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js', // 产生的 chunk 自定义命名
+        entryFileNames: 'assets/js/[name]-[hash].js', // 指定 chunks 的入口文件匹配模式
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]' // 自定义构建结果中的静态资源名称，资源文件像 字体，图片等
+      }
+    }
+  },
   plugins: [
     vue(),
     AutoImport({
